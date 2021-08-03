217ad4b9a78661450d6e75e1ddb65e0fe95616cb
Dorian Buck <[dorianbuck@gmail.com]>
2021-08-03 15:21:42 +0200

    made 1st function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,15 @@
-alert('this works')
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function add(num1, num2) {
+  const result = num1 + num2;
+  alert('The result is ' + result);
+}
+
+add(18, 219);
+
+currentResult = (currentResult + 9) * 3;
+
+let calculationDescription = `( + ${defaultResult} + 7) (0+13)`;
+let errorMessage = "An error" + "occured";
+outputResult(currentResult, calculationDescription);
---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,11 @@
+const userInput = 15;
+let result = (18 + userInput);
+
+result = (result * 3);
+result = (result - 15);
+result = (result + 1000);
+
+alert(userInput);
+alert(result);
+
+
